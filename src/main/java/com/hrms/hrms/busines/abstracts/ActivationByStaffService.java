e44bc1795192d4700d5fa7c60e4846ed27ecbb54
Batu Akdogan <77547523+batuakdogan@users.noreply.github.com>
2021-07-23 10:48:12 +0300

    Delete src/main/java/com/hrms/hrms/busines directory

--- src/main/java/com/hrms/hrms/busines/abstracts/ActivationByStaffService.java
+++
@@ -1,9 +0,0 @@
-package com.alihocaoglu.hrms.busines.abstracts;
-
-import com.alihocaoglu.hrms.core.utilities.results.Result;
-import com.alihocaoglu.hrms.entities.concretes.Employer;
-
-public interface ActivationByStaffService {
-    void createActivationByStaff(Employer employer);
-    Result activateEmployer(int employerId,int staffId);
-}
--- src/main/java/com/hrms/hrms/busines/abstracts/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.alihocaoglu.hrms.busines.abstracts;
-
-import com.alihocaoglu.hrms.entities.concretes.User;
-
-public interface EmailService {
-    void sendVerifyEmail(User user,String code);
-}

b331e35edcbfa7c1420e7ff8dc1ffacdc339ee88
Batu Akdogan <77547523+batuakdogan@users.noreply.github.com>
2021-07-23 07:46:43 +0000

    updated

--- src/main/java/com/hrms/hrms/dataAccess/abstracts/JobAdDao.java
+++ src/main/java/com/hrms/hrms/dataAccess/abstracts/JobAdDao.java
@@ -16,6 +16,11 @@ public interface JobAdDao extends JpaRepository<JobAd,Integer> {
     List<JobAd> findByActiveTrueAndEmployer_Id(int id);
 
 
+
+    @Query("From JobAd where confirmed = false")
+    List<JobAd> getWaitingJobAdvertisements();
+
+
     @Query("Select j from com.alihocaoglu.hrms.entities.concretes.JobAd j where ((:#{#filter.cityId}) IS NULL OR j.city.id IN (:#{#filter.cityId}))"
         +" and ((:#{#filter.jobPositionId}) IS NULL OR j.jobPosition.id IN (:#{#filter.jobPositionId}))"
         +" and ((:#{#filter.workPlaceId}) IS NULL OR j.workPlace.id IN (:#{#filter.workPlaceId}))"
